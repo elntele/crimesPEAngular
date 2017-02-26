d56c776aadce2dec9aa0b440f493adbf387386f3
Jefferson Cabral <jcabral1500@gmail.com>
2017-02-26 20:04:30 -0300

    Compilação status OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//import { AppComponent }  from './app.component';
+import { AppComponent }  from './app.component';
 import { PaginaInicialComponent }  from './pagina-inicial.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [  PaginaInicialComponent ],
-  bootstrap:    [ PaginaInicialComponent ]
+  imports: [ 
+  	BrowserModule,
+  	RouterModule.forRoot([
+  		{
+  			path: '',
+  			redirectTo: '/paginaInicial',
+  			pathMatch: 'full'
+  		},
+  		{
+  			path: 'paginaInicial',
+  			component: PaginaInicialComponent
+  		}
+  	])
+   ],
+  declarations: [ AppComponent, PaginaInicialComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
